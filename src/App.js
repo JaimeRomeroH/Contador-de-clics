60ce218404ccb65a9ca71c0bac412b0ba17c603b
JaimeRomero <jaime_im@outlook.com>
2022-06-22 15:58:45 -0500

    Se importo el componente Boton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import './App.css';
-import logo from "./imagenes/app-logo.jpg"
+import logo from "./imagenes/app-logo.jpg";
+import Boton from './componentes/Boton';
+
 function App() {
+  const manejarClic = () =>{
+    console.log("Clic");
+  }
+  
+  const reiniciarContador = () =>{
+    console.log("Reiniciar");
+  }
+
   return (
     <div className="App">
       <div className="logo-contenedor">
@@ -9,7 +19,16 @@ function App() {
          alt="Logo"/>
       </div>
       <div className="contador-principal">
-
+         <Boton
+           texto="Clic"
+           esBotonDeClic={true}
+           manejarClic={manejarClic} 
+         /> 
+         <Boton
+          texto="Reiniciar"
+          esBotonDeClic={false} 
+          manejarClic={reiniciarContador} 
+         /> 
       </div>
       
     </div>
